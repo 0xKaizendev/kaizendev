3e76033d2c28f39b0c6f2b025ec7e4a9e9f364b3
0xKaizendev <admin@kaizendev.me>
2025-03-12 16:02:13 -0700

    add resend api key and update email

--- actions/send-email.ts
+++ actions/send-email.ts
@@ -5,6 +5,7 @@ import { Resend } from "resend";
 import ContactFormEmail from "@/components/sections/contact/_components/contact-form-email";
 import { getErrorMessage, validateString } from "@/lib/utils";
 
+console.log(process.env.RESEND_API_KEY);
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
@@ -26,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: ["anthoniotheruiner@gmail.com"],
+      to: ["admin@kaizendev.me"],
       subject: "Message from contact form | PORTFOLIO",
       replyTo: senderEmail,
       react: React.createElement(ContactFormEmail, {
